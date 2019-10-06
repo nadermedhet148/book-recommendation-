88337ece475c40119c2edbf6dd2952ab834d82eb
nadermedhet148 <nadermedhet142@gmail.com>
2019-10-06 12:17:37 +0200

    get selected book all  related books with sorting

--- system.py
+++ system.py
@@ -1,7 +1,4 @@
-# check list
-# 1- get book ratings average
-# 2- get relation with all books
-# 3- sort related book by relations then rating
+
 # %%
 import pandas as pd
 import numpy as np
@@ -22,29 +19,41 @@ reviews_per_book = ratings.groupby(['book_id'])[
     'rating'].mean()
 # reviews_per_book.head(100)
 # %%
-# get book's readers , example The Hunger Games
-Hunger_Games = books.loc[books['original_title'] == 'The Hunger Games']
-Hunger_Games_readers = ratings.loc[ratings['book_id']
-                                   == Hunger_Games['id'][0]].sort_values('rating', ascending=False)
-# get users  books
-all_readers_books = {}
-for index, reader in Hunger_Games_readers.iterrows():
-    # get all realted books
-    reader_books = ratings.loc[ratings['user_id']
-                               == reader['user_id']]
-    # group books by id and get its average rating and count
-    for index, book in reader_books.iterrows():
-        if book['book_id'] in all_readers_books.keys():
-            all_readers_books[book['book_id']
-                              ]['count'] = all_readers_books[book['book_id']]['count'] + 1
-        else:
-            all_readers_books[book['book_id']] = {
-                "count": 1,
-                "book_id": book['book_id'],
-                "average_rating": reviews_per_book[book['book_id']]
-            }
 
 
-print(all_readers_books)
+# %%
+# get relted books to selected one
+def getReltedBooks(book_name):
+    # get book's readers , example The Hunger Games
+    book = books.loc[books['original_title'] == book_name]
+    book_readers = ratings.loc[ratings['book_id']
+                               == book['id'][0]].sort_values('rating', ascending=False)
+    # get users  books
+    all_readers_books = {}
+    for index, reader in book_readers.iterrows():
+        # get all realted books
+        reader_books = ratings.loc[ratings['user_id']
+                                   == reader['user_id']]
+        # group books by id and get its average rating and count
+        for index, book in reader_books.iterrows():
+            if book['book_id'] in all_readers_books.keys():
+                all_readers_books[book['book_id']
+                                  ]['count'] = all_readers_books[book['book_id']]['count'] + 1
+            else:
+                all_readers_books[book['book_id']] = {
+                    "count": 1,
+                    "book_id": book['book_id'],
+                    "average_rating": reviews_per_book[book['book_id']],
+                    "book": books.loc[books['book_id'] == book['book_id']]
+                }
+    # sort books to get high priority first
+    sorted_books = sorted(all_readers_books.values(),
+                          key=lambda y: (y['count'] + y['average_rating']),  reverse=True)
+
+    return sorted_books
+
+
+print(getReltedBooks('The Hunger Games')[0: 100])
+
 
 # %%
